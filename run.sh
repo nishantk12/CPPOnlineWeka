2f0d3884487fcca26dfa3e5912da886095703159
milindjindal <milindjindal@gmail.com>
2015-12-07 14:13:41 -0700

    Comments
    added some comments to the file

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 clear
-
+# This is a file to do the initial setup of the code. Before running this 
+# file, the user needs to set the following two variables in the system.
+# With the variables of LIBPATH and CLASSPATH. LIBPATH and CLASSPATH
+# variables are needed for any further use of the code, as an include. The
+# execution of this file results in creation of sym-links in the directory:
+# /usr/include/ which means that whenever this code is compiled the JNI lib
+# files are not needed to be included in the G++ command.
+#
+# Wheras if the code is to be used then the compilation should be supplied 
+# with two library parameters : -ljvm and -L{$JAVA_HOME/jre/lib/amd64/server/}
+#
 ##########################################################################
 # VARIABLES TO BE SET BY THE USER
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export WORK_DIRECTORY=/home/ni/Robotics/CPP/Robotics/
 export WEKA_HOME=/opt/weka-3-6-13
 ##########################################################################
 
 # EXTENDED VARIABLES
+export WORK_DIRECTORY=${PWD}
 export LIBPATH=$JAVA_HOME/jre/lib/amd64/server/
 export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/:$JAVA_HOME/jre/lib/amd64/server
 export CLASSPATH=$CLASSPATH:$WORK_DIRECTORY:$WEKA_HOME/weka.jar
@@ -44,18 +54,18 @@ if [ -a executable ]
 fi
 
 if  [ ! -f /usr/include/jni.h ] 
-then
-	ln -s /usr/lib/jvm/java-8-oracle/include/jni.h /usr/include/jni.h
+	then
+		ln -s /usr/lib/jvm/java-8-oracle/include/jni.h /usr/include/jni.h
 fi
 
 if  [ ! -f /usr/include/jni_md.h ]	
-then
-	ln -s /usr/lib/jvm/java-8-oracle/include/linux/jni_md.h /usr/include/jni_md.h
+	then
+		ln -s /usr/lib/jvm/java-8-oracle/include/linux/jni_md.h /usr/include/jni_md.h
 fi
 
 if  [ ! -f /usr/include/GenericClassifierWeka.h ]	
-then
-	ln -s ${PWD}/GenericClassifierWeka.h /usr/include/GenericClassifierWeka.h
+	then
+		ln -s ${PWD}/GenericClassifierWeka.h /usr/include/GenericClassifierWeka.h
 fi
 
 g++ MultiClass.cpp -o executable -L${LIBPATH} -ljvm
